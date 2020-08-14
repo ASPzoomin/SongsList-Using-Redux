7b5a8aab9c5c114ce0d0430142b37499e0124908
sai pavan <pavanammula@gmail.com>
2020-08-14 18:06:21 +0530

    wired up the provider

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { selectedSong } from "../actions";
 const App = () => {
   return <div>App</div>;
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducers from "./reducers";
 import App from "./components/App";
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <Provider>
+    <App store={createStore(reducers)} />
+  </Provider>,
+  document.querySelector("#root")
+);
