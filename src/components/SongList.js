e286e8a08d422ee2bed7440946dd92be4d09de4e
sai pavan <pavanammula@gmail.com>
2020-08-14 22:12:34 +0530

    wired react with connect and Providers

---
+++ src/components/SongList.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { selectedSong } from "../actions";
+class SongList extends Component {
+  renderedList() {
+    return this.props.songs.map((song) => {
+      return (
+        <div className='item' key={song.title}>
+          <div className='right floated content'>
+            <button
+              className='ui button primary'
+              onClick={() => this.props.selectedSong(song)}
+            >
+              Select
+            </button>
+          </div>
+          <div className='content'>{song.title}</div>
+        </div>
+      );
+    });
+  }
+  render() {
+    return <div className='ui divided list'>{this.renderedList()}</div>;
+  }
+}
+const mapStateToProps = (state) => {
+  return { songs: state.songs };
+};
+export default connect(mapStateToProps, { selectedSong })(SongList);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
-import reducers from "./reducers";
 import App from "./components/App";
+import reducers from "./reducers";
+import { createStore } from "redux";
 ReactDOM.render(
-  <Provider>
-    <App store={createStore(reducers)} />
+  <Provider store={createStore(reducers)}>
+    <App />
   </Provider>,
   document.querySelector("#root")
 );
